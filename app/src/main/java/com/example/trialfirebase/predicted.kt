9f5a31cd19e39b22e299bd1b7fbf525e9247de40
VJen <57284701+VJen@users.noreply.github.com>
2023-06-21 15:26:18 +0700

    Update predicted.kt

--- app/src/main/java/com/example/trialfirebase/predicted.kt
+++ app/src/main/java/com/example/trialfirebase/predicted.kt
@@ -12,14 +12,16 @@ import androidx.appcompat.app.AppCompatActivity
 
 class predicted : AppCompatActivity() {
 
-    private var containerDataList: Array<*>? = null
-    private var currentContainerLocation: MutableList<String> = mutableListOf()
+    private var containerDataList: Array<*>? = null // Holds the container data
+    private var currentContainerLocation: MutableList<String> = mutableListOf() // Keeps track of displayed container locations
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.predicted)
-
+        setContentView(R.layout.predicted) // Set the layout file for the activity
+        
+        // Get the container data list from the intent extras
         containerDataList = intent.getSerializableExtra("containerDataList") as? Array<*>
+        // Initialize UI components from the layout file
         val tableLayout = findViewById<TableLayout>(R.id.tableLayout)
 
         val button1 = findViewById<Button>(R.id.button1)
@@ -31,7 +33,8 @@ class predicted : AppCompatActivity() {
         val button7 = findViewById<Button>(R.id.button7)
         val button8 = findViewById<Button>(R.id.button8)
         val buttonact = findViewById<Button>(R.id.activate)
-
+        
+        // Set click listeners for the buttons
         button1.setOnClickListener {
             displayContainerData("1", tableLayout)
         }
@@ -56,31 +59,38 @@ class predicted : AppCompatActivity() {
         button8.setOnClickListener {
             displayContainerData("8", tableLayout)
         }
+        // Set click listener for the "activate" button to start a new activity
         buttonact.setOnClickListener{
             val intent = Intent(this, RobotArm::class.java)
             startActivity(intent)
         }
     }
 
+    // Function to display container data for a specific location in the table layout
     private fun displayContainerData(containerLocation: String, tableLayout: TableLayout) {
-
+        
+        // Skip if the same location is already displayed
         if (currentContainerLocation.contains(containerLocation)) {
             return  // Skip if the same location is already displayed
         }
-
+        
+        // Add the current location to the list of displayed locations
         currentContainerLocation.add(containerLocation)
+        // Iterate over the container data list
         containerDataList?.forEach { containerData ->
             if (containerData is Map<*, *>) {
                 val location = containerData["location"].toString()
                 if(location == containerLocation) {
+                    // Extract container data fields
                     val id = containerData["id"].toString()
                     val dest = containerData["destinations"].toString()
                     val type = containerData["cargo_type"].toString()
                     val weight = containerData["cargo_weight"].toString()
-
-
+                    
+                    // Create a new row in the table layout
                     val row = TableRow(this)
 
+                    // Create TextViews for each container data field
                     val idTextView = createTextView(id)
                     row.addView(idTextView)
 
@@ -98,11 +108,13 @@ class predicted : AppCompatActivity() {
                     val locationTextView = createTextView(location)
                     row.addView(locationTextView)
 
+                    // Add the row to the table layout
                     tableLayout.addView(row, LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
                 }
             }
         }
     }
+    // Function to create a TextView with specified text and layout parameters
     private fun createTextView(text: String): TextView {
         val textView = TextView(this)
         textView.text = text
