015705a4674c0a43e4dd2a833e4cdaa84aef23e1
VJen <57284701+VJen@users.noreply.github.com>
2023-06-21 15:47:33 +0700

    Update SignUpActivity.kt

--- app/src/main/java/com/example/trialfirebase/SignUpActivity.kt
+++ app/src/main/java/com/example/trialfirebase/SignUpActivity.kt
@@ -18,12 +18,13 @@ class SignUpActivity : AppCompatActivity() {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        firebaseAuth = FirebaseAuth.getInstance()
+        firebaseAuth = FirebaseAuth.getInstance() // Initialize FirebaseAuth instance
 
         binding.textView.setOnClickListener {
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
         }
+        // Set click listener for "Sign In" text view
         binding.button.setOnClickListener {
             val email = binding.emailEt.text.toString()
             val pass = binding.passET.text.toString()
@@ -31,20 +32,24 @@ class SignUpActivity : AppCompatActivity() {
 
             if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
                 if (pass == confirmPass) {
-
+                    // Attempt to create a new user with the provided email and password
                     firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
                         if (it.isSuccessful) {
+                            // Sign up successful, navigate to the sign-in activity
                             val intent = Intent(this, SignInActivity::class.java)
                             startActivity(intent)
                         } else {
+                            // Sign up failed, display error message
                             Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
 
                         }
                     }
                 } else {
+                    // Display a toast message if the passwords do not match
                     Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
                 }
             } else {
+                // Display a toast message if any of the fields are empty
                 Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
 
             }
