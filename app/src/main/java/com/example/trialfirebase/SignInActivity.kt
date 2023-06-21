6e2aa03474714abe89ee4d45864399c66cfaa962
VJen <57284701+VJen@users.noreply.github.com>
2023-06-21 15:45:10 +0700

    Update SignInActivity.kt

--- app/src/main/java/com/example/trialfirebase/SignInActivity.kt
+++ app/src/main/java/com/example/trialfirebase/SignInActivity.kt
@@ -18,28 +18,33 @@ class SignInActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        firebaseAuth = FirebaseAuth.getInstance()
+        firebaseAuth = FirebaseAuth.getInstance() // Initialize FirebaseAuth instance
+        // Set click listener for "Sign Up" text view
         binding.textView.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
 
+        // Set click listener for "Sign In" button
         binding.button.setOnClickListener {
             val email = binding.emailEt.text.toString()
             val pass = binding.passET.text.toString()
 
             if (email.isNotEmpty() && pass.isNotEmpty()) {
-
+                // Attempt to sign in with the provided email and password
                 firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
                     if (it.isSuccessful) {
+                        // Sign in successful, navigate to the main activity
                         val intent = Intent(this, MainActivity::class.java)
                         startActivity(intent)
                     } else {
+                        // Sign in failed, display error message
                         Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
 
                     }
                 }
             } else {
+                // Display a toast message if any of the fields are empty
                 Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
 
             }
